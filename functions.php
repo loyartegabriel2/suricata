951fcca338ee6132651f2124a6d0df8f949de4d3
Gabriel <25462565+loyartegabriel2@users.noreply.github.com>
2021-07-16 00:50:50 -0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+
+//Custom background
+//include ('functions/custom-background.php');
+
+//Agrega el modulo de logo custom a la personalizacion
+//include ('functions/custom-logo.php');
+
+//Agrega modulo de imagen destacada
+//include ('functions/post-thumbnails.php');
+
+//Carga el sidebar
+//include ('functions/custom-sidebar.php');
+
+//Navbar
+//include ('functions/menu-nav.php');
+
+//Custom header
+//include ('functions/custom-header.php');
+
+//Cargar el CSS
+function suricata_load_styles() {
+wp_register_style('theme_style', get_stylesheet_uri(), '', '1.0', 'all');
+wp_enqueue_style('theme_style');}
+add_action('wp_enqueue_scripts','suricata_load_styles');
+
+// Add theme support for selective refresh for widgets.
+add_theme_support( 'customize-selective-refresh-widgets' );
+
+// Add support for Block Styles.
+add_theme_support( 'wp-block-styles' );
+
+// Add support for full and wide align images.
+add_theme_support( 'align-wide' );
+
+
+
+	
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bullhost</title> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+
+<body class="home blog logged-in admin-bar no-customize-support custom-background wp-custom-logo">
+
+<!-- NAVBAR-->
+
+<div id="global-container" class="global-container">
+<nav class="navbar navbar-dark bg-dark">
+<!--- Logo custom --->
+<a href="https://bullhost.com.ar"><?php 
+   $custom_logo_id = get_theme_mod( 'custom_logo' );
+   $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+      ?>
+<img src="<?php echo $image[0]; ?>" lt="Bullhost" class="logo"></a>
+<!--- Logo custom --->
+
+
+	<!--- Termiba navbar --->
+
+			
+			
+
+	<?php get_sidebar();?>	
+<?php get_footer();?>
